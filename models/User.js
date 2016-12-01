6eb836e0148763fffe7bbb352afd34796435fe10
Wose <Jacksonteather@hotmail.com>
2016-12-01 13:08:12 -0500

    Missed file in last commit

--- models/User.js
+++ models/User.js
@@ -6,11 +6,14 @@ var Types = keystone.Field.Types;
  * ==========
  */
 var User = new keystone.List('User', {
-	track: true
+	track: true,
+	autokey: { path: 'userId', from: 'name', unique: true },
+	map: { name: 'name.full' }
 });
 
 User.add({
 	name: { type: Types.Name, required: true, index: true },
+	avatar: { type: Types.CloudinaryImage, folder: 'user/avatars', publicID: 'userId', autoCleanup : true, select : true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
 }, 'Permissions', {
@@ -20,6 +23,9 @@ User.add({
 // Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function () {
 	return this.isAdmin;
+});
+User.schema.virtual('avatarTag').get(function () {
+	return this._.avatar.tag();
 });
 
 
