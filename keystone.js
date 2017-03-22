675947d158eccbc918da1cc9bcfbc0f74635d7ab
Wose <Jacksonteather@hotmail.com>
2017-03-22 17:11:18 -0400

    Removed server side Sass rendering and updated keystone
    4.0.0-beta-5 merged with #nested-lists

--- keystone.js
+++ keystone.js
@@ -17,7 +17,6 @@ keystone.init({
 	'name': 'UOIT Virtual Tour',
 	'brand': 'UOIT Virtual Tour',
 	// templates
-	'sass': 'public',
 	'static': 'public',
 	'static options': { maxAge: '200d' },
 	'favicon': 'public/icons-cf06493c3c6774a5839aed2c41a0a2a4/favicon.ico',
